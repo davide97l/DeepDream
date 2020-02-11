b58f5500cbef5b4fa0ade09d67db96baf76c1452
Davide <davide97ls@gmail.com>
2020-02-11 13:04:19 +0800

    added resize file_jpg.py and improve code

--- DeepDreamGenerator.py
+++ DeepDreamGenerator.py
@@ -5,23 +5,22 @@ import PIL.Image
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
-model_fn = 'models/tensorflow_inception_graph.pb'
-
 # creating TensorFlow session and loading the model
 graph = tf.Graph()
+model_fn = 'models/tensorflow_inception_graph.pb'  # path to the saved model
 sess = tf.InteractiveSession(graph=graph)  # install itself as the default session on construction
 # load the protobuf file from the disk and parse it to retrieve the un-serialized graph_def
 with tf.gfile.FastGFile(model_fn, 'rb') as f:  # give access to the file containing the trained-model
     graph_def = tf.GraphDef()  # serialized version of graph
     graph_def.ParseFromString(f.read())  # read the protobuf file and generate a graph
 t_input = tf.placeholder(np.float32, name='input')  # define the input tensor
-imagenet_mean = 117.0
-t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)  # normalize the input image
+imagenet_mean = 117.0  # imagenet dataset mean used to normalize the input images
+t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)  # normalize the input images
 tf.import_graph_def(graph_def, {'input': t_preprocessed})  # import a graph and eventually set some parameters
 
 
-# convert np array to PIL image and return it
 def get_img_from_array(img):
+    """Convert np.array to PIL image and return it"""
     a = np.uint8(np.clip(img, 0, 1) * 255)
     return PIL.Image.fromarray(a)
 
@@ -43,11 +42,10 @@ def tffunc(*argtypes):
     return wrap
 
 
-# Helper function that uses TF to resize an image
 def resize(img, size):
+    """Helper function that uses TF to resize an image"""
     img = tf.expand_dims(img, 0)
     return tf.image.resize_bilinear(img, size)[0,:,:,:]
-
 resize = tffunc(np.float32, np.int32)(resize)
 
 
@@ -60,11 +58,11 @@ def calc_grad_tiled(img, t_grad, tile_size=512):
     sx, sy = np.random.randint(sz, size=2)
     img_shift = np.roll(np.roll(img, sx, 1), sy, 0)
     grad = np.zeros_like(img)
-    for y in range(0, max(h-sz//2, sz),sz):
-        for x in range(0, max(w-sz//2, sz),sz):
-            sub = img_shift[y:y+sz,x:x+sz]
-            g = sess.run(t_grad, {t_input:sub})
-            grad[y:y+sz,x:x+sz] = g
+    for y in range(0, max(h-sz//2, sz), sz):
+        for x in range(0, max(w-sz//2, sz), sz):
+            sub = img_shift[y:y+sz, x:x+sz]
+            g = sess.run(t_grad, {t_input: sub})
+            grad[y:y+sz, x:x+sz] = g
     return np.roll(np.roll(grad, -sx, 1), -sy, 0)
 
 
@@ -85,13 +83,13 @@ def deepdream(t_obj, img, iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
 
     # generate details octave by octave
     for octave in range(octave_n):
+        print('octave: {}'.format(octave))
         if octave > 0:
             hi = octaves[-octave]
             img = resize(img, hi.shape[:2]) + hi
         for i in range(iter_n):
             g = calc_grad_tiled(img, t_grad)
             img += g * (step / (np.abs(g).mean() + 1e-7))
-            print('octave: {}, iteration: {}'.format(octave, i))
 
         pil_img = get_img_from_array(img / 255.0)
         generated_images.append(pil_img)
@@ -99,67 +97,68 @@ def deepdream(t_obj, img, iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
     return generated_images
 
 
-# display net layers
-layers = [op.name for op in graph.get_operations() if op.type=='Conv2D' and 'import/' in op.name]
-feature_nums = [int(graph.get_tensor_by_name(name+':0').get_shape()[-1]) for name in layers]
-print('Layers: {}'.format(layers))
-print('Total number of feature channels:', sum(feature_nums))
-
-
-objfuncs = {
-            # layers
-            'effect4-3': tf.square(T('mixed4c')),  # wolves
+objfuncs = {'effect4-3': tf.square(T('mixed4c')),  # wolves
             'effect4-1': tf.square(T('mixed4a')),  # eyes
             'effect4-2': tf.square(T('mixed4b')),  # dogs
             'effect4-4': tf.square(T('mixed4d')),  # snakes
             'effect5-1': tf.square(T('mixed5a')),  # birds
             'effect5-2': tf.square(T('mixed4b')),  # dogs
-            # filters
             'flowers': T('mixed4d_3x3_bottleneck_pre_relu')[:, :, :, 139],
             'wolves': T('mixed4d_3x3_bottleneck_pre_relu')[:, :, :, 138],
             'homes': T('mixed4d_3x3_bottleneck_pre_relu')[:, :, :, 65],
             }
 
-# optional parameters
-show = True
-save = True
-use_default_image = False  # random noise
-# algorithm parameters
-iter_n = [30]  # how many gradient ascent step per octave
-octave_n = 4  # number of multiple scales of the image onto apply gradient ascent
-octave_scale = 1.4  # how much resize the image with each octave
-# input parameters
-input_path = 'images'
-output_path = 'deepdreams'
-image_paths = ['tsinghua.jpg']
-effect = ['effect4-1', 'effect4-2', 'effect4-3', 'effect4-4', 'flowers']
+if __name__ == "__main__":
 
-for image_path in image_paths:
-    print(image_path)
-    img_name = image_path.split(".")[0]
-    img = 0
-    if use_default_image:
-        # start with a gray image with a little noise
-        img = np.random.uniform(size=(499, 499, 3)) + 100.0
-    else:
-        img = PIL.Image.open(os.path.join(input_path, image_path))
-        img = np.float32(img)
+    # display net layers
+    layers = [op.name for op in graph.get_operations() if op.type == 'Conv2D' and 'import/' in op.name]
+    feature_nums = [int(graph.get_tensor_by_name(name + ':0').get_shape()[-1]) for name in layers]
+    print('Layers: {}'.format(layers))
+    print('Total number of feature channels:', sum(feature_nums))
 
-    generated_images = []
-    for i, e in enumerate(effect):
-        print("effect: {}".format(i))
-        for n in iter_n:
-            # apply deep dream algorithm
-            images = deepdream(objfuncs[e], img, iter_n=n, octave_n=octave_n, octave_scale=octave_scale)
-            generated_images = generated_images + images
+    # optional parameters
+    show = False  # show images once processed
+    save = True  # save images to folder
+    use_default_image = False  # if True generates images from noise
 
-    for i, img_ in enumerate(generated_images):
-        if show:
-            img_.show()
-        if save:
-            save_path = os.path.join(os.getcwd(), output_path, img_name)
-            if not os.path.exists(save_path):
-                os.makedirs(save_path)
-            img_.save(os.path.join(os.getcwd(), save_path,
-                                   img_name + '_' + str(i)
-                                   + '_' + str(iter_n[i % (len(iter_n))]) + '.jpg'))
+    # algorithm parameters
+    iter_n = [100]  # how many gradient ascent step per octave
+    octave_n = 2  # number of multiple scales of the image onto apply gradient ascent
+    octave_scale = 1.4  # how much resize the image with each octave
+
+    # input parameters
+    input_path = 'images/'
+    output_path = 'deepdreams/'
+    # see objfuncs dict to see possible effects
+    effect = ['effect4-1', 'effect4-2', 'effect4-3', 'effect4-4', 'effect5-1', 'flowers', 'homes']
+
+    input_dir = os.listdir(input_path)
+    for item in input_dir:
+        if os.path.isfile(os.path.join(input_path, item)) and item.endswith("jpg"):
+            print("Processing image: " + item)
+            img_name = item.split(".")[0]
+            if use_default_image:
+                # start with a gray image with a little noise
+                img = np.random.uniform(size=(499, 499, 3)) + 100.0
+            else:
+                img = PIL.Image.open(os.path.join(input_path, item))
+                img = np.float32(img)
+
+            generated_images = []
+            for i, eff in enumerate(effect):
+                print("effect: {}".format(i))
+                for n in iter_n:
+                    # apply deep dream algorithm
+                    images = deepdream(objfuncs[eff], img, iter_n=n, octave_n=octave_n, octave_scale=octave_scale)
+                    generated_images = generated_images + images
+
+            for i, img_ in enumerate(generated_images):
+                if show:
+                    img_.show()
+                if save:
+                    save_path = os.path.join(os.getcwd(), output_path, img_name)
+                    if not os.path.exists(save_path):
+                        os.makedirs(save_path)
+                    img_.save(os.path.join(os.getcwd(), save_path,
+                                           img_name + '_' + str(i)
+                                           + '_' + str(iter_n[i % (len(iter_n))]) + '.jpg'))
---
+++ resize_jpg.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import os
+import argparse
+
+
+def resize(path, t=800):
+    input_dir = os.listdir(path)
+    for item in input_dir:
+        if os.path.isfile(path+item) and item.endswith("jpg"):
+            im = Image.open(path+item)
+            w, h = im.size
+            if w > t or h > t:
+                if w >= h:
+                    im = im.resize((t, int(h / (w/t))), Image.ANTIALIAS)
+                else:
+                    im = im.resize((int(w / (h/t)), t), Image.ANTIALIAS)
+            f, e = os.path.splitext(path+item)
+            im.save(f + '_res.jpg', 'JPEG', quality=100)
+
+
+if __name__ == "__main__":
+    """
+    Resize all jpg images in a specific path keeping the original rateo
+    Example: python resize_jpg.py -i images/ -t 1000
+    Effect: All images whose width or height is above 1000 will be resized: (2000, 1500) -> (1000, 750)
+    """
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", type=str, required=True,
+                    help="path to input folder containing the images")
+    ap.add_argument("-t", "--threshold", type=int, default=800,
+                    help="threshold above the which resize an image")
+    args = vars(ap.parse_args())
+
+    resize(args["input"], args["threshold"])
+
+    print("Image resized")
